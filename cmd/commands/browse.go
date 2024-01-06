bcecee9f5d8b33232ab4c744587a26aece73d38e
fadyat <fadyat@icloud.com>
2024-01-06 16:21:49 +0300

    feat: pump browse project if not task_id specified

--- cmd/commands/browse.go
+++ cmd/commands/browse.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"github.com/fadyat/pump/internal"
+	"github.com/fadyat/pump/internal/driver/options"
 	"github.com/fadyat/pump/pkg"
 	"github.com/spf13/cobra"
 	"runtime"
@@ -13,22 +14,22 @@ func BrowseTask(cfg *internal.Config) *cobra.Command {
 
 	return &cobra.Command{
 		Use:     "browse",
-		Short:   "Browse specific task info in the browser",
+		Short:   "Browse project/specified task",
 		Aliases: []string{"info", "open", "view"},
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			if cfg.Driver != "asana" {
 				return fmt.Errorf("asana driver is only supported")
 			}
 
-			if len(args) == 0 {
-				return fmt.Errorf("task id is required")
+			if len(args) > 0 {
+				taskID = args[0]
 			}
 
-			taskID = args[0]
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			var asanaURL = fmt.Sprintf("https://app.asana.com/0/0/%s", taskID)
+			var opts = options.AsanaDriverFromMap(cfg.GetDriverOpts())
+			asanaURL := fmt.Sprintf("https://app.asana.com/0/%s/%s", opts.ProjectID, taskID)
 			return pkg.RunCmd(getBrowserCommand(), asanaURL)
 		},
 		SilenceUsage: true,
