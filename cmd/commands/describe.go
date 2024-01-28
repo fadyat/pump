0f46c33f2d6c9d5045f60129ec5283ec237e0bde
fadyat <fadyat@icloud.com>
2024-01-28 16:40:54 +0300

    feat: ValidArgsFunction is really cool

--- cmd/commands/describe.go
+++ cmd/commands/describe.go
@@ -4,16 +4,50 @@ import (
 	"fmt"
 	"github.com/fadyat/pump/internal"
 	"github.com/fadyat/pump/internal/driver"
+	"github.com/fadyat/pump/internal/model"
 	"github.com/spf13/cobra"
 )
+
+// todo: reuse this function in other commands
+func newService(cfg *internal.Config) (internal.Service, error) {
+	d, err := driver.New(cfg.Driver, cfg.GetDriverOpts())
+	if err != nil {
+		return nil, err
+	}
+
+	return internal.NewSvc(d), nil
+}
+
+func autoCompleteArgs[T any](args []T, take func(arg T) string) []string {
+	values := make([]string, len(args))
+	for idx, arg := range args {
+		values[idx] = take(arg)
+	}
+
+	return values
+}
 
 func DescribeTask(cfg *internal.Config) *cobra.Command {
 	var taskID string
 
 	return &cobra.Command{
-		Use:     "describe",
+		Use:     "describe [task_id]",
 		Short:   "Describe specified task",
 		Aliases: []string{"edit"},
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			svc, err := newService(cfg)
+			if err != nil {
+				return nil, cobra.ShellCompDirectiveError
+			}
+
+			tasks, err := svc.Get()
+			if err != nil {
+				return nil, cobra.ShellCompDirectiveError
+			}
+
+			idTaker := func(task *model.Task) string { return task.ID }
+			return autoCompleteArgs(tasks, idTaker), cobra.ShellCompDirectiveNoFileComp
+		},
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			if cfg.Driver != "asana" {
 				return fmt.Errorf("asana driver is only supported")
@@ -30,16 +64,12 @@ func DescribeTask(cfg *internal.Config) *cobra.Command {
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			driv, err := driver.New(cfg.Driver, cfg.GetDriverOpts())
+			svc, err := newService(cfg)
 			if err != nil {
 				return err
 			}
 
-			var (
-				svc    = internal.NewSvc(driv)
-				editor = internal.NewEditor("Name", "Description")
-			)
-
+			editor := internal.NewEditor("Name", "Description")
 			task, err := svc.GetByID(taskID)
 			if err != nil {
 				return err
